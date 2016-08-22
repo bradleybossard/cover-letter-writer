c6439eb9133a61cf1d781f6c8836c8dcf07d5dff
Bradley Bossard <bradleybossard@gmail.com>
2016-08-22 19:49:41 +0000

    Adding positiveRole

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var app = angular.module('myApp', [])
   $scope.skill1 = "diverse technical background";
   $scope.skill2 = "attention to detail";
   $scope.skill3 = "great communication skills";
+  $scope.positiveRole = "an excellent addition";  // a strong candidate
   $scope.position = "Software Engineer";
   $scope.closing = "Regards";
   $scope.signature = "Bradley";
 
+/*
+frontend and fullstack experience
+combined with my desire to wear many hats and work in many different aspects of software and product development
+
+*/
 });
