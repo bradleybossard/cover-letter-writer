1dca8c04b3a8be31f37d60d7e77492fb2309e35c
Bradley Bossard <bradleybossard@gmail.com>
2016-08-22 14:11:54 +0000

    Adding more fields

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ var app = angular.module('myApp', [])
 .controller('myCtrl', function($scope) {
   $scope.companyName= "Google";
   $scope.skill1 = "diverse technical background";
+  $scope.skill2 = "attention to detail";
+  $scope.skill3 = "great communication skills";
+  $scope.position = "Software Engineer";
+  $scope.closing = "Regards";
+  $scope.signature = "Bradley";
+
 });
