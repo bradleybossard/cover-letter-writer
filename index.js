febe439c845b6b3118c7b5321a51a3351fc57cfa
Bradley Bossard <bradleybossard@gmail.com>
2016-08-24 23:59:35 +0000

    Adding more fields and reduced HTML complexity

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var app = angular.module('myApp', [])
 .controller('myCtrl', function($scope) {
 
   $scope.skills = [
-  { id: 0, name: 'diverse technical background'},
-  { id: 1, name: 'attention to detail'},
-  { id: 2, name: 'great communication skills'},
-  //{ id: , name: ''}
+    { id: 0, name: 'diverse technical background'},
+    { id: 1, name: 'attention to detail'},
+    { id: 2, name: 'great communication skills'},
+    //{ id: , name: ''}
   ];
 
   $scope.roles = [
-  { id: 0, name: 'an excellent addition'},
-  { id: 1, name: 'a strong candidate'},
-  ]
+    { id: 0, name: 'an excellent addition'},
+    { id: 1, name: 'a strong candidate'},
+  ];
+
+  $scope.closings = [
+    { id: 0, name: 'Sincerely'},
+    { id: 1, name: 'Regards'},
+  ];
 
   $scope.companyName= "Google";
   $scope.skill1 = $scope.skills[0].name;
@@ -19,7 +24,7 @@ var app = angular.module('myApp', [])
   $scope.skill3 = $scope.skills[2].name;
   $scope.role = $scope.roles[0].name;
   $scope.position = "Software Engineer";
-  $scope.closing = "Regards";
+  $scope.closing = $scope.closings[0].name;
   $scope.signature = "Bradley";
 
 /*
