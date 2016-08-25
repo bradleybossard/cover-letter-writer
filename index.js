cc105278597ae4a4564d82c6faeb76d021b53d0b
Bradley Bossard <bradleybossard@gmail.com>
2016-08-25 00:38:20 +0000

    Adding more content

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ var app = angular.module('myApp', [])
     { id: 0, name: 'diverse technical background'},
     { id: 1, name: 'attention to detail'},
     { id: 2, name: 'great communication skills'},
+    { id: 3, name: 'frontend/fullstack experience'},
+    { id: 4, name: 'desire to wear many hats'}, 
     //{ id: , name: ''}
   ];
 
   $scope.roles = [
-    { id: 0, name: 'an excellent addition'},
-    { id: 1, name: 'a strong candidate'},
+    { id: 0, name: 'an excellent addition to your team as a'},
+    { id: 1, name: 'a strong candidate for the opening of'},
+    { id: 2, name: 'a well qualified candidate for the position of'},
   ];
 
   $scope.closings = [
     { id: 0, name: 'Sincerely'},
     { id: 1, name: 'Regards'},
+    { id: 2, name: 'Best'},
+    { id: 3, name: 'Cheers'},
   ];
 
   $scope.companyName= "Google";
@@ -26,10 +31,4 @@ var app = angular.module('myApp', [])
   $scope.position = "Software Engineer";
   $scope.closing = $scope.closings[0].name;
   $scope.signature = "Bradley";
-
-/*
-frontend and fullstack experience
-combined with my desire to wear many hats and work in many different aspects of software and product development
-
-*/
 });
